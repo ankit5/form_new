c2306903ee4885f6d1024828c27ed8105593ad2c
ankit5 <vermankit5@gmail.com>
2023-05-05 17:27:22 +0530

    update

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -838,10 +838,21 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-$databases['default']['default'] = array (
+/*$databases['default']['default'] = array (
   'database' => 'colart_new',
   'username' => 'root',
   'password' => 'admin',
+  'prefix' => '',
+  'host' => 'localhost',
+  'port' => '3306',
+  'namespace' => 'Drupal\\mysql\\Driver\\Database\\mysql',
+  'driver' => 'mysql',
+  'autoload' => 'core/modules/mysql/src/Driver/Database/mysql/',
+);*/
+$databases['default']['default'] = array (
+  'database' => 'ankitver_form_new',
+  'username' => 'ankitver_drupal',
+  'password' => '8y{a(o-IrhQ~',
   'prefix' => '',
   'host' => 'localhost',
   'port' => '3306',
